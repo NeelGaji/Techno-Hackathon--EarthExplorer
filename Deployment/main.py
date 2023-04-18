796cddf2a06a0fcd0a557a9597b063914ee6d84a
ayushkumar05 <ayushk0520@gmail.com>
2023-04-18 13:14:34 +0530

    form integration

--- Deployment/main.py
+++ Deployment/main.py
@@ -1,47 +1,59 @@
-from flask import Flask, request
-import pandas as pd
-import requests
-from datetime import datetime
+import csv
+import datetime
+from geopy.geocoders import Nominatim
+from flask import Flask, request, render_template,jsonify
 
 app = Flask(__name__)
+geolocator = Nominatim(user_agent='crime_app')
 
-# Load the crime dataset
-crime_df = pd.read_csv('crime_data.csv')
+@app.route('/')
+def landing():
+    return render_template('index.html')
+
+@app.route('/prediction', methods = ['POST'])
+def prediction():
+    return render_template('routing.html')
+
+@app.route('/fetch_recent_incident')
+def fetch_recent():
+    with open('file.csv', 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        next(csv_reader)  # Skip the header row
+        second_row = next(csv_reader)  # Read the second row
+
+    return jsonify(second_row)
+
 
 @app.route('/update_crime_dataset', methods=['POST'])
 def update_crime_dataset():
-    # Get the form input data
-    print(request.form)
-    new_crime = {
-        'type': request.form['type'],
-    }
-    print(new_crime)
+    # Get the crime type from the form data
+    crime_type = request.form['type']
+
+    # Get the user's location using the geolocation API of the browser
+    latitude = request.headers.get('X-Forwarded-For')
+    longitude = request.headers.get('X-Forwarded-Proto')
+
+    # Reverse geocode the latitude and longitude into an address
+    location = geolocator.reverse(f'{latitude}, {longitude}')
+    address = location.address
+
     # Get the current date and time
-    now = datetime.now()
-    new_crime['date'] = now.strftime('%Y-%m-%d')
-    new_crime['time'] = now.strftime('%H:%M:%S')
-    
-    # Get the user's location using the HTML5 Geolocation API
-    user_location = request.form['location']
-    lat, long = user_location.split(',')
-    
-    # Geocode the user's location to get the address
-    response = requests.get(f'https://nominatim.openstreetmap.org/reverse?lat={lat}&lon={long}&format=json')
-    address = response.json()['display_name']
-    
-    # Add the user's location and address to the new crime
-    new_crime['latitude'] = lat
-    new_crime['longitude'] = long
-    new_crime['address'] = address
-    
-    # Add the new crime to the dataset
-    crime_df = crime_df.append(new_crime, ignore_index=True)
-    
-    # Save the updated dataset to a file
-    crime_df.to_csv('crime_dataset.csv', index=False)
-    
-    # Return a success message
-    return {'message': 'Crime dataset updated successfully.'}, 200
+    current_date_time = datetime.datetime.now()
+
+    # Get the year, month, day, hour, and minute from the current date and time
+    year = current_date_time.year
+    month = current_date_time.month
+    day = current_date_time.day
+    hour = current_date_time.hour
+    minute = current_date_time.minute
+
+    # Add the new data to the dataset
+    with open('crime_dataset.csv', 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow([latitude, longitude, address, crime_type, month, year, day, hour, minute])
+
+    return 'Data updated successfully'
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,port=6000)
